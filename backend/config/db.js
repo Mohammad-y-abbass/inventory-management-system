434fffc1a2e2d124e5f2d98ecd936981a5d44c3f
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-02-21 16:16:54 +0200

    handle db error

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectToDb = async () => {
   mongoose
     .connect(process.env.MONGO_URI)
-    .then(() => console.log('Connected to MongoDB'));
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('Failed to connect to MongoDB', err));
 };
 
 module.exports = connectToDb;
