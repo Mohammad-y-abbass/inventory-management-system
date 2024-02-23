6f702f5d9d0fb6c4b62b2c3f95e7ff6e24d83cfc
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-02-23 16:41:00 +0200

    create register controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,45 @@
-const registerUser = (req, res) => {
-  try{
-    
-  }catch(err) {
+const asyncHandler = require('express-async-handler');
+const User = require('../models/user');
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error('Please fill in all fields');
   }
-};
+
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error('Password must be at least 6 characters');
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const newUSer = await User.create({
+    username,
+    email,
+    password,
+  });
+
+  if (newUser) {
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      photo: newUser.photo,
+      bio: newUser.bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 
 module.exports = {
   registerUser,
